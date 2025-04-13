b5527c124d8f758bac7702840f9135827f994f59
NikolaiProgramist <nikluk2005@mail.ru>
2025-04-13 15:44:17 +0300

    Update layouts/app.blade.php for the Tailwind CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,15 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="csrf-param" content="_token" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
+        @vite('resources/css/app.css')
     </head>
     <body>
-        <div class="container mt-4">
+        <div>
             <div>
                 @yield('content')
             </div>
         </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
