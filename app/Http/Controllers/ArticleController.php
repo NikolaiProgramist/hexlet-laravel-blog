12de05827e03a809779953790ac68f1af9b3a854
NikolaiProgramist <nikluk2005@mail.ru>
2025-04-13 16:46:20 +0300

    Add paginate page count 3 to the ArticleController.php and fix route names to the web.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,7 +9,7 @@ class ArticleController extends Controller
 {
     public function index(): View
     {
-        $articles = Article::paginate();
+        $articles = Article::paginate(3);
         return view('article.index', compact('articles'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', [PageController::class, 'index'])->name('index');
+Route::get('/', [PageController::class, 'index'])->name('page.index');
 
-Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/about', [PageController::class, 'about'])->name('page.about');
 
 Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
