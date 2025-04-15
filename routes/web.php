3daa50ebceccc92394bd2b57e3d74d0d4ddc26a2
NikolaiProgramist <nikluk2005@mail.ru>
2025-04-15 15:39:52 +0300

    Fix lint

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', [PageController::class, 'index'])->name('page.index');
+Route::get('/', [PageController::class, 'index'])
+    ->name('page.index');
 
-Route::get('/about', [PageController::class, 'about'])->name('page.about');
+Route::get('/about', [PageController::class, 'about'])
+    ->name('page.about');
 
-Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('/articles', [ArticleController::class, 'index'])
+    ->name('articles.index');
 
-Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
+Route::get('/articles/{id}', [ArticleController::class, 'show'])
+    ->name('articles.show');
