391dc57fd2525ded128087fc3c9728068a7dddae
NikolaiProgramist <nikluk2005@mail.ru>
2025-04-22 14:14:40 +0300

    Install @rails/ujs dependency and include it to the app.js, layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="csrf-param" content="_token" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <header>
